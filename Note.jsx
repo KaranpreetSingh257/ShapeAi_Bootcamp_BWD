494f9cb14f50095e9746bc8eda49d44a9a375ee1
Karanpreet Singh <85469090+KaranpreetSingh257@users.noreply.github.com>
2021-06-07 23:02:24 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div>
+            <h1>Javascript and React.JS</h1>
+            <h2>This was an amazing bootcamp taken up by Shaurya Sinha we covered everything from scratch including Javascript and React.JS,HTML. </h2>
+        </div>
+    );
+}
+
+export default Note;
